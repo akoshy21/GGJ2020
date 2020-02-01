965d24c87d6ba033772885f05693af9a6ab635dd
akoshy21 <ak6429@nyu.edu>
2020-01-31 20:33:06 -0500

    no message

---
+++ Assets/Scripts/Client.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Client
+{
+    public GameObject client;
+    public GameObject patient;
+
+    public Item[] favoriteItems;
+}
