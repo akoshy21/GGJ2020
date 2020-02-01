d4c474185a13ca90fa6d1bded9b87389090acea5
akoshy21 <ak6429@nyu.edu>
2020-01-31 20:06:02 -0500

    sup

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item
+{
+    public enum size { Small, Medium, Large }
+    public size itemSize;
+
+    public Item()
+    {
+    }
+
+    public Item(size itemSz)
+    {
+        itemSize = itemSz;
+    }
+}
